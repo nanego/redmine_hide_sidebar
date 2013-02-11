c50893ec2c9b5b5d6681b6bc3ee03eaa1cc125df
Berk Demirkır <berk.demirkir@ries.com.tr>
2013-02-11 13:53:28 +0200

    Branding fixes. New version: 0.0.2

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require_dependency 'sidebar_hook_listener'
 
 Redmine::Plugin.register :sidebar_hide do
   name 'Sidebar Hide Plugin'
-  author 'Furkan Yavuz'
+  author 'Ries Technologies'
   description 'This plugin provides ability to hide sidebar'
-  version '0.0.1'
-  url 'http://www.redmine.org/projects/redmine/wiki/Plugin_Tutorial'
-  author_url 'http://www.redmine.org/projects/redmine/wiki/Plugin_Tutorial'
+  version '0.0.2'
+  url 'https://github.com/ries-tech/sidebar_hide'
+  author_url 'https://github.com/ries-tech/sidebar_hide'
 end
