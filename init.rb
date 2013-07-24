fcdc0dd5e671493ad22ceaddde45021b85086c4e
Berk Demirkır <bdemirkir@gmail.com>
2013-07-24 13:06:20 +0300

    Version 0.0.4

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :sidebar_hide do
   name 'Sidebar Hide Plugin'
   author 'Ries Technologies'
   description 'This plugin provides ability to hide sidebar'
-  version '0.0.3'
+  version '0.0.4'
   url 'https://github.com/ries-tech/sidebar_hide'
   author_url 'https://github.com/ries-tech/sidebar_hide'
 end
