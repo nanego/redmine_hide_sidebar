1ea2094b90841ae8f31332eb327e0fddcb76ee49
Alexander Meindl <a.meindl@alphanodes.com>
2016-07-08 12:50:35 +0200

    move css and js to html header

--- lib/sidebar_hook_listener.rb
+++ lib/sidebar_hook_listener.rb
@@ -1,3 +1,8 @@
 class SidebarHookListener < Redmine::Hook::ViewListener
-	render_on :view_layouts_base_content, :partial => "sidebar/hideButton_partial"
+	render_on :view_layouts_base_content, :partial => 'sidebar/hideButton_partial'
+
+  def view_layouts_base_html_head(_context = {})
+    stylesheet_link_tag('sidebar_hide', :plugin => 'sidebar_hide', :media => 'all') +
+      javascript_include_tag('sidebar_hide', :plugin => 'sidebar_hide')
+  end
 end
