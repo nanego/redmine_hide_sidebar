ebe6cd794d12f1b3f8fcc618d518bd1ecc1646a3
draschba <clemens.draschba@thyssenkrupp.com>
2022-06-13 11:52:04 +0200

    fix to comply with redmine 5.0.x

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require_dependency 'sidebar_hook_listener'
+require File.expand_path('lib/sidebar_hook_listener', __dir__)
 
 Redmine::Plugin.register :redmine_hide_sidebar do
   name 'Sidebar Hide Plugin'
