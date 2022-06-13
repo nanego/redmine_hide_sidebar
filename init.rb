ed4e465d5bc317c161b7ca1cb4260de078b87630
Vincent Robert <vincent.robert@nanego.com>
2022-06-13 15:31:57 +0200

    Improve compatibility with Redmine 5

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-require_dependency 'sidebar_hook_listener'
+require_relative 'lib/sidebar_hook_listener'
 
 Redmine::Plugin.register :redmine_hide_sidebar do
   name 'Sidebar Hide Plugin'
